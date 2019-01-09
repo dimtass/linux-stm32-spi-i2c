3e99449bfa3ea458ff5b9bf112413740e65c8144
Dimitris Tassopoulos <dimitris.tassopoulos@native-instruments.de>
2019-01-09 13:05:12 +0100

    Removed build folder

--- stm32-i2c-spi/build-stm32/CMakeFiles/linux-stm32-spi-i2c.bin.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/linux-stm32-spi-i2c.bin"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/linux-stm32-spi-i2c.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- stm32-i2c-spi/build-stm32/CMakeFiles/write.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/write"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/write.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
