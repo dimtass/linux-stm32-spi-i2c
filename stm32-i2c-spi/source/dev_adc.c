326f5b7a6b307ca9d57567c2c19abb036cf6949f
Dimitris Tassopoulos <dimtass@gmail.com>
2019-01-15 23:31:39 +0100

    adada

--- stm32-i2c-spi/source/dev_adc.c
+++ stm32-i2c-spi/source/dev_adc.c
@@ -7,165 +7,132 @@
 
 #include "dev_adc.h"
 
-struct adc_input m_adc_input[] = {
-	[0] = {GPIOA, GPIO_Pin_0},
-	[1] = {GPIOA, GPIO_Pin_1},
-	[2] = {GPIOA, GPIO_Pin_2},
-	[3] = {GPIOA, GPIO_Pin_3},
-	[4] = {GPIOA, GPIO_Pin_4},
-	[5] = {GPIOA, GPIO_Pin_5},
-	[6] = {GPIOA, GPIO_Pin_6},
-	[7] = {GPIOA, GPIO_Pin_7},
-	[8] = {GPIOB, GPIO_Pin_0},
-	[9] = {GPIOB, GPIO_Pin_1},
+LIST_HEAD(adc_channel_list);
+
+static struct adc_controller m_adc_controller[] = {
+    [DEV_ADC1] = {ADC1, RCC_APB2Periph_ADC1},
+    [DEV_ADC2] = {ADC2, RCC_APB2Periph_ADC2},
 };
 
-#define ADC1_DR_Address    ((uint32_t)0x4001244C)
-#define ADC2_DR_Address    ((uint32_t)0x4001284C)
-//#define ADC3_DR_Address    ((uint32_t)0x40013C4C)
+struct adc_input m_adc_input[] = {
+	[ADC_CH0] = {GPIOA, GPIO_Pin_0, RCC_APB2Periph_GPIOA},
+	[ADC_CH1] = {GPIOA, GPIO_Pin_1, RCC_APB2Periph_GPIOA},
+	[ADC_CH2] = {GPIOA, GPIO_Pin_2, RCC_APB2Periph_GPIOA},
+	[ADC_CH3] = {GPIOA, GPIO_Pin_3, RCC_APB2Periph_GPIOA},
+	[ADC_CH4] = {GPIOA, GPIO_Pin_4, RCC_APB2Periph_GPIOA},
+	[ADC_CH5] = {GPIOA, GPIO_Pin_5, RCC_APB2Periph_GPIOA},
+	[ADC_CH6] = {GPIOA, GPIO_Pin_6, RCC_APB2Periph_GPIOA},
+	[ADC_CH7] = {GPIOA, GPIO_Pin_7, RCC_APB2Periph_GPIOA},
+	[ADC_CH8] = {GPIOB, GPIO_Pin_0, RCC_APB2Periph_GPIOB},
+	[ADC_CH9] = {GPIOB, GPIO_Pin_1, RCC_APB2Periph_GPIOB},
+};
+
+static uint8_t adc1_curr_channel = 0;
+static uint8_t adc2_curr_channel = 0;
 
 #define ADC_EXISTS(ADC, ITTERATOR) (ADC->channel == ITTERATOR)
 
-static struct dev_adc_module * dev_adc1 = NULL;
-static struct dev_adc_module * dev_adc2 = NULL;
-
-/* Prototypes */
-static inline struct dev_adc * dev_adc_get_next_channel(struct dev_adc_module * dev);
-
-void dev_adc_module_init(struct dev_adc_module * dev)
+int adc_init_channel(struct adc_channel * ch, enum en_adc_dev adc_device,
+					enum en_adc_channel_num channel_num,
+					enum en_adc_mode mode, adc_cb_t callback)
 {
-	ADC_InitTypeDef ADC_InitStructure;
-	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
+	int ret = 0;
 
-	/* initialize list for ADC channels */
-	INIT_LIST_HEAD(&dev->adc_ch_list);
+	/* Handle errors */
+	if (!ch)
+		return -1;
+	if (adc_device >= DEV_ADC_END)
+		return -2;
+	if (adc_device >= ADC_CH_END)
+		return -3;
 
-	RCC_ADCCLKConfig(RCC_PCLK2_Div6);
-	/* Enable ADC1 and GPIOA clock */
-	if (dev->adc == ADC1) {
-		dev_adc1 = dev;
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
-	}
-	else if (dev->adc == ADC2) {
-		dev_adc2 = dev;
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2 | RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
-	}
+	if (SystemCoreClock > 72000000)
+		RCC_ADCCLKConfig(RCC_PCLK2_Div6)
+	else
+		RCC_ADCCLKConfig(RCC_PCLK2_Div4);
 
-	/* ADC1 configuration ------------------------------------------------------*/
-	ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
-	ADC_InitStructure.ADC_ScanConvMode = DISABLE;
-	ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
-	ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-	ADC_InitStructure.ADC_NbrOfChannel = 1;
-	ADC_Init(dev->adc, &ADC_InitStructure);
+	list_add(&ch->list, &adc_channel_list);
+	
+	/* copy parameters */
+	ch->ch_num = channel_num;
+	ch->mode = mode;
+	ch->adc_cb = callback;
+	ch->ready = 0;
 
-	/* Enable ADC1 EOC interrupt */
-	ADC_ITConfig(dev->adc, ADC_IT_EOC, ENABLE);
+	ch->controller->adc_num = adc_device;
+	ch->controller->adc = &m_adc_controller[adc_device];
+	ch->controller->input = &m_adc_input[channel_num];
 
-	/* Enable ADC1 */
-	ADC_Cmd(dev->adc, ENABLE);
+	/* Enable ADCx and GPIOx clock */
+	uint32_t rcc = ch->controller->apb_periph | ch->controller->input->apb_periph;
+	RCC_APB2PeriphClockCmd(rcc, ENABLE);
+
+	/* ADC configuration */
+	ch->conf.ADC_Mode = ADC_Mode_Independent;
+	ch->conf.ADC_ScanConvMode = DISABLE;
+	ch->conf.ADC_ContinuousConvMode = ENABLE;
+	ch->conf.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+	ch->conf.ADC_DataAlign = ADC_DataAlign_Right;
+	ch->conf.ADC_NbrOfChannel = 1;
+	ADC_Init(ch->controller->adc, &ch->conf);
+
+	/* Enable ADCx EOC interrupt */
+	ADC_ITConfig(ch->controller->adc, ADC_IT_EOC, ENABLE);
+
+	/* Enable ADCx */
+	ADC_Cmd(ch->controller->adc, ENABLE);
 	/* Enable ADC1 reset calibration register */
-	ADC_ResetCalibration(dev->adc);
-	/* Check the end of ADC1 reset calibration register */
-	while(ADC_GetResetCalibrationStatus(dev->adc));
-	/* Start ADC1 calibration */
-	ADC_StartCalibration(dev->adc);
-	/* Check the end of ADC1 calibration */
-	while(ADC_GetCalibrationStatus(dev->adc));
+	ADC_ResetCalibration(ch->controller->adc);
+	/* Check the end of ADCx reset calibration register */
+	while(ADC_GetResetCalibrationStatus(ch->controller->adc));
+	/* Start ADCx calibration */
+	ADC_StartCalibration(ch->controller->adc);
+	/* Check the end of ADCx calibration */
+	while(ADC_GetCalibrationStatus(ch->controller->adc));
+	/* Default sample time 13.5 cycles */
+	ADC_RegularChannelConfig(ch->controller->adc, ch->ch_num, 1, ADC_SampleTime_13Cycles5);
 
+	NVIC_InitTypeDef NVIC_InitStructure; // this is used to configure the NVIC (nested vector interrupt controller)
 	NVIC_InitStructure.NVIC_IRQChannel = ADC1_2_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5; // this sets the priority group of the interrupts
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;// this sets the subpriority inside the group
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;	// the USART1 interrupts are globally enabled
 	NVIC_Init(&NVIC_InitStructure);	// the properties are passed to the NVIC_Init function which takes care of the low level stuff
+
+	return ret;
 }
 
-
-void dev_adc_start(struct dev_adc_module * dev)
+int adc_start(struct adc_channel * ch)
 {
-	if (!dev->curr_channel) dev->curr_channel = dev_adc_get_next_channel(dev);
-	TRACEL(TRACE_LEVEL_ADC, ("ADC:start:%d\n", dev->curr_channel->channel));
-	if (dev->curr_channel) {
-		/* Enable ADC2 EOC interrupt */
-		ADC_ITConfig(dev->adc, ADC_IT_EOC, ENABLE);
-		/* Start ADC1 Software Conversion */
-		ADC_SoftwareStartConvCmd(dev->adc, ENABLE);
-	}
+	if (!ch)
+		return -1;
+
+	TRACEL(TRACE_LEVEL_ADC, ("ADC%d.%d start\n", ch->controller->adc_num, ch->ch_num));
+	/* Set channel */
+	ADC_RegularChannelConfig(ch->controller->adc, ch->ch_num, 1, sample_time);
+	/* Enable ADCx EOC interrupt */
+	ADC_ITConfig(ch->controller->adc, ADC_IT_EOC, ENABLE);
+	/* Start ADCx Software Conversion */
+	ADC_SoftwareStartConvCmd(ch->controller->adc, ENABLE);
+
+	ch->enable = 1;
+	ch->avg->value = 0;
+	ch->avg->shift_cntr = 0;
 }
 
-
-void dev_adc_stop(struct dev_adc_module * dev)
+void adc_stop(struct adc_channel * ch)
 {
-	/* Start ADC1 Software Conversion */
-	ADC_SoftwareStartConvCmd(dev->adc, DISABLE);
-	/* Enable ADC2 EOC interrupt */
-	ADC_ITConfig(dev->adc, ADC_IT_EOC, DISABLE);
+
+	ch->avg->value = 0;
+	ch->avg->shift_cntr = 0;
 }
 
-
-static inline struct dev_adc * dev_adc_find_channel(struct dev_adc_module * dev, uint8_t ch)
+void adc_set_sample_speed(struct adc_channel * ch, uint8_t sample_time)
 {
-	if (!list_empty(&dev->adc_ch_list)) {
-		struct dev_adc * ch_it = NULL;
-		list_for_each_entry(ch_it, &dev->adc_ch_list, list) {
-			if ADC_EXISTS(ch_it, ch) {
-				/* found */
-				return(ch_it);
-			}
-		}
-	}
-	return NULL;
+	ch->sample_time = sample_time;
 }
 
-
-void* dev_adc_probe(struct dev_adc * ch)
-{
-	/* Only add channel if not already found */
-	if (!ch->owner || !ch) return NULL;
-	struct dev_adc_module * dev = ch->owner;
-	if (dev_adc_find_channel(dev, ch->channel)) return NULL;
-
-	struct dev_adc * new_adc = (struct dev_adc *) malloc(sizeof(struct dev_adc));
-	memcpy(new_adc, ch, sizeof(struct dev_adc));
-
-	/* init dev head list */
-	INIT_LIST_HEAD(&new_adc->list);
-	/* Add to led_list */
-	list_add(&new_adc->list, &dev->adc_ch_list);
-	TRACEL(TRACE_LEVEL_ADC, ("ADC:add->%d\n", new_adc->channel));
-	return new_adc;
-}
-
-
-void dev_adc_remove(struct dev_adc * ch)
-{
-	if (!ch->owner || !ch) return;
-	struct dev_adc * found_ch = dev_adc_find_channel(ch->owner, ch->channel);
-	if (found_ch) {
-		/* remove */
-		list_del(&found_ch->list);
-		free(found_ch);
-		TRACEL(TRACE_LEVEL_ADC, ("ADC:delete->%d\n", ch->channel));
-	}
-}
-
-
-uint8_t dev_adc_set_channel(struct dev_adc_module * dev, uint8_t ch)
-{
-	struct dev_adc * found_ch = dev_adc_find_channel(dev, ch);
-	if (found_ch) {
-		/* ADC1 regular channel0 configuration */
-		ADC_RegularChannelConfig(dev->adc, found_ch->channel, 1, ADC_SampleTime_28Cycles5);
-
-		/* Enable ADC2 EOC interrupt */
-		ADC_ITConfig(dev->adc, ADC_IT_EOC, ENABLE);
-	}
-	return(!!found_ch);
-}
-
-
-uint8_t dev_adc_enable_channel(struct dev_adc_module * dev, uint8_t ch, uint8_t enable)
+uint8_t adc_enable_channel(struct adc_channel * ch, uint8_t enable)
 {
 	struct dev_adc * found_ch = dev_adc_find_channel(dev, ch);
 	if (found_ch) {
@@ -174,13 +141,12 @@ uint8_t dev_adc_enable_channel(struct dev_adc_module * dev, uint8_t ch, uint8_t 
 	return(!!found_ch);
 }
 
-static inline struct dev_adc * dev_adc_get_next_channel(struct dev_adc_module * dev)
+static inline struct adc_channel * adc_find(ADC_TypeDef * adc, uint8_t channel)
 {
-	if (!list_empty(&dev->adc_ch_list)) {
-		struct dev_adc * ch;
-		list_for_each_entry(ch, &dev->adc_ch_list, list) {
-			if (ch->enable && !ch->ready) {
-				TRACEL(TRACE_LEVEL_ADC, ("ADC:next->%d\n", ch->channel));
+	if (!list_empty(&adc_ch_list)) {
+		struct adc_channel * ch_it;
+		list_for_each_entry(ch_it, &ch->adc_ch_list, list) {
+			if ((ch->controller->adc == adc) && (ch->ch_num == channel))) {
 				return ch;
 			}
 		}
@@ -188,22 +154,29 @@ static inline struct dev_adc * dev_adc_get_next_channel(struct dev_adc_module * 
 	return NULL;
 }
 
-
-uint8_t dev_adc_reset_channels(struct dev_adc_module * dev)
+static inline struct adc_channel * dev_adc_get_next_channel()
 {
-	uint8_t num_of_enabled_channels = 0;
-	if (!list_empty(&dev->adc_ch_list)) {
-		struct dev_adc * ch;
-		list_for_each_entry(ch, &dev->adc_ch_list, list) {
-			if (ch->enable) {
-				ch->ready = 0;
-				num_of_enabled_channels++;
+	if (!list_empty(&adc_ch_list)) {
+		struct adc_channel * ch_it;
+		list_for_each_entry(ch_it, &ch->adc_ch_list, list) {
+			if (ch->enable && !ch->ready) {
+				TRACEL(TRACE_LEVEL_ADC, ("ADC:next->%d\n", ch->ch_num));
+				return ch;
 			}
 		}
 	}
-	return(num_of_enabled_channels);
+	return NULL;
 }
 
+void dev_adc_reset_channel(struct adc_channel * ch)
+{
+	ch->ready = 0;
+	ch->value = 0;
+	if (ch->avg.shift) {
+		ch->avg.value = 0;
+		ch->avg.shift_cntr = 0;
+	}
+}
 
 void dev_adc_update(struct dev_adc_module * dev)
 {
@@ -218,15 +191,28 @@ void dev_adc_update(struct dev_adc_module * dev)
 void ADC1_2_IRQHandler(void)
 {
 	if (ADC_GetITStatus(ADC1, ADC_IT_EOC) != RESET) {
-		/* save value */
-		dev_adc_stop(dev_adc1);
-		dev_adc1->curr_channel->value = ADC_GetConversionValue(ADC1);
-		TRACEL(TRACE_LEVEL_ADC, ("ADC1:value[%d]->%d\n", dev_adc1->curr_channel->channel, dev_adc1->curr_channel->value));
-		dev_adc1->curr_channel->ready = 1;
-		dev_adc1->curr_channel = dev_adc_get_next_channel(dev_adc1);
-		if (dev_adc1->curr_channel) {
-			dev_adc_set_channel(dev_adc1, dev_adc1->curr_channel->channel);
-			dev_adc_start(dev_adc1);
+		/* Stop ADCx Software Conversion */
+		ADC_SoftwareStartConvCmd(ADC1, DISABLE);
+		/* Disable ADCx EOC interrupt */
+		ADC_ITConfig(ADC1, ADC_IT_EOC, DISABLE);
+		/* Retrieve channel */
+		struct adc_channel * ch = adc_find(ADC1, adc1_curr_channel);
+		if (ch) {
+			/* save value */
+			if (ch->avg.shift) {
+
+			}
+			else {
+				ch->value = ADC_GetConversionValue(ADC1);
+				ch->ready = 1;
+			}
+			TRACEL(TRACE_LEVEL_ADC, ("ADC1:value[%d]->%d\n", dev_adc1->curr_channel->channel, dev_adc1->curr_channel->value));
+	
+			adc1_curr_channel = dev_adc_get_next_channel(dev_adc1);
+			if (adc1_curr_channel) {
+				ch = adc_find(ADC1, adc1_curr_channel);
+				adc_start(ch);
+			}
 		}
 		ADC_ClearITPendingBit(ADC1, ADC_IT_EOC);
 	}
@@ -238,8 +224,7 @@ void ADC1_2_IRQHandler(void)
 		dev_adc2->curr_channel->ready = 1;
 		dev_adc2->curr_channel = dev_adc_get_next_channel(dev_adc2);
 		if (dev_adc2->curr_channel) {
-			dev_adc_set_channel(dev_adc2, dev_adc2->curr_channel->channel);
-			dev_adc_start(dev_adc2);
+			adc_start(ch);
 		}
 		ADC_ClearITPendingBit(ADC2, ADC_IT_EOC);
 	}
--- stm32-i2c-spi/source/inc/dev_adc.h
+++ stm32-i2c-spi/source/inc/dev_adc.h
@@ -41,52 +41,71 @@ enum en_adc_dev {
     DEV_ADC_END
 };
 
+enum en_adc_channel_num {
+	ADC_CH0 = 0,
+	ADC_CH1, ADC_CH2, ADC_CH3, ADC_CH4, ADC_CH5,
+	ADC_CH6, ADC_CH7, ADC_CH8, ADC_CH9, ADC_CH_END
+};
+
 enum en_adc_mode {
 	DEV_ADC_MODE_SINGLE,
-	DEV_ADC_MODE_POLLING    // creates IRQ for the host
+	DEV_ADC_MODE_POLLING    // triggers a conversion as soon the previous is done
 };
 
-typedef int (*adc_cb_t)(struct adc_device * adc, uint16_t value);
-
-static struct adc_controller m_adc_controller[] = {
-    [DEV_ADC1] = {ADC1, 0x4001244C, RCC_APB2Periph_ADC1, },
-    [DEV_ADC2] = {},
-};
+typedef int (*adc_cb_t)(struct adc_channel * adc, uint16_t value);
 
 struct adc_input {
 	GPIO_TypeDef    *port;
 	uint16_t 		pin;
+    uint32_t       	apb_periph;
 };
 
 struct adc_controller {
+	uint8_t 		adc_num;
 	ADC_TypeDef     *adc;
+	uint32_t 		apb_periph;
 	struct adc_input *input;
-    uint32_t        address;
-    uint32_t        apb_periph;
 };
 
-struct adc_device {
-    uint16_t        ch;
-    uint8_t         enable;
+struct adc_noise_avg {
+	uint32_t 		value;
+	uint8_t			shift;
+	uint8_t			shift_cntr;
+};
+
+struct adc_channel {
+    uint16_t        ch_num;
     enum en_adc_mode mode;
 	uint8_t 		ready;
+	uint8_t			enable;
 	volatile uint16_t value;
+	uint8_t 		sample_time;
 	adc_cb_t		adc_cb;	// callback
 
 	ADC_InitTypeDef conf;
-    struct adc_dma  *dma;
+	struct adc_noise_avg avg;
     struct adc_controller *controller;
 	struct list_head list;
 };
 
+int adc_init_channel(struct adc_channel * ch, enum en_adc_dev adc_device,
+					enum en_adc_channel_num channel_num,
+					enum en_adc_mode mode, adc_cb_t callback);
+/**
+ * This should only be a power of two, because it shifts the adc value
+ */
+int adc_avg_conf(uint8_t avg_shift);
 
-void adc_init(struct adc_device * dev, enum en_adc_mode mode, adc_cb_t callback);
-void* dev_adc_probe(struct dev_adc * adc_dev_arr);
-void dev_adc_remove(struct dev_adc * adc_dev_arr);
-uint8_t dev_adc_set_channel(struct dev_adc_module * dev, uint8_t ch);
-uint8_t dev_adc_enable_channel(struct dev_adc_module * dev, uint8_t ch, uint8_t enable);
-uint8_t dev_adc_reset_channels(struct dev_adc_module * dev);
-void dev_adc_update(struct dev_adc_module * dev);
+/**
+ * @param[in] sample_time	see ADC_SampleTime_* in stm32f10x_adc.h
+ */
+void adc_set_sample_speed(struct adc_channel * ch, uint8_t sample_time);
+
+uint8_t adc_set_channel(struct adc_channel * dev, uint8_t ch);
+uint8_t adc_enable_channel(struct adc_channel * dev, uint8_t enable);
+uint8_t adc_disable_channel(struct adc_channel * dev);
+void adc_reset_channel(struct adc_channel * ch);
+// void adc_update(struct adc_channel * dev);
 
 
 #endif /* DEV_ADC_H_ */
